362ee9fdc58ac8e2fc6a9ccd92c7cbcd61b32c45
John Godley <johng75@gmail.com>
2016-06-10 14:16:46 +0100

    Remove level of components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import 'aframe';
+import { Scene } from 'aframe-react';
 import { connect } from 'react-redux';
-
-import SelectorPage from '../SelectorPage';
+import Camera from '../Camera'
+import Cursor from '../Cursor'
+import WorldSelection from '../WorldSelector/world-selection';
+import World from '../World';
 
 const App = React.createClass( {
 	render: function() {
+		const { worlds, worldFocus, currentWorld } = this.props;
+
 		return (
-			<div>
-				<SelectorPage { ...this.props }/>
-			</div>
+			<Scene>
+				<Camera position="-1 0 3.5">
+					<Cursor isSelected={ worldFocus !== -1 }/>
+				</Camera>
+
+				{ currentWorld === -1 ? <WorldSelection worlds={ worlds }/> : <World world={ worlds[currentWorld] }/> }
+			</Scene>
 		);
 	}
 } );
 
 // Which part of the Redux global state does our component want to receive as props?
 function mapStateToProps( state ) {
-	return state;
+	const { world } = state;
+
+	return {
+		worlds: world.worlds,
+		worldFocus: world.worldFocus,
+		currentWorld: world.currentWorld,
+	};
 }
 
 function mapDispatchToProps( dispatch ) {
